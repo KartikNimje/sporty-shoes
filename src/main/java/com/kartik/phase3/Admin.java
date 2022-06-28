e69f117277bef1870ddca971243f241d24b91d13
Kartik Nimje <kartiknimje1@gmail.com>
2022-06-28 12:26:37 +0530

    first commit

---
+++ src/main/java/com/kartik/phase3/Admin.java
@@ -0,0 +1,28 @@
+package com.kartik.phase3;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int ad_id;
+	private String ad_fname;
+	private String ad_lname;
+	private String ad_phone;
+	private String ad_mail;
+	private String ad_pass;
+	private String ad_con;
+	private String ad_gen;
+	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String log_date;
+	
+}
---
+++ src/main/webapp/ad_get_report.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Kartik</title>
+</head>
+<h1>Get here</h1>
+<body>
+<form action="get_report">
+<input type="date" name="sell_date">
+<input type="submit" value="Generate Report">
+</form>
+</body>
+</html>
